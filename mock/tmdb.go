8f66982ce398c4cefa82701540777ee739b857c5
Florian Ehmke <ehmkeflorian@gmail.com>
2019-06-28 07:17:57 +0200

    tests: add mock package with mocks for tvdb and tmdb

---
+++ mock/tmdb.go
@@ -0,0 +1,16 @@
+package mock
+
+import "github.com/florianehmke/plexname/tmdb"
+
+type tmdbClient struct {
+	response *tmdb.SearchResponse
+	err      error
+}
+
+func NewMockTMDB(response *tmdb.SearchResponse, err error) tmdb.Client {
+	return &tmdbClient{response, err}
+}
+
+func (c *tmdbClient) Search(query string, year int, page int) (*tmdb.SearchResponse, error) {
+	return c.response, c.err
+}
---
+++ mock/tvdb.go
@@ -0,0 +1,16 @@
+package mock
+
+import "github.com/florianehmke/plexname/tvdb"
+
+type tvdbClient struct {
+	response *tvdb.SearchResponse
+	err      error
+}
+
+func NewMockTVDB(response *tvdb.SearchResponse, err error) tvdb.Client {
+	return &tvdbClient{response, err}
+}
+
+func (c *tvdbClient) Search(query string) (*tvdb.SearchResponse, error) {
+	return c.response, c.err
+}
